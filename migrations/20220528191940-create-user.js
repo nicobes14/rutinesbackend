4c0eec882c8198d960eef02b1746a8dd3f556c89
Nicolas Bes <nicobes14@gmail.com>
2022-05-29 21:09:11 -0300

    update sequelize migrations

--- migrations/20220528191940-create-user.js
+++ migrations/20220528191940-create-user.js
@@ -9,10 +9,13 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       rutineId: {
         type: Sequelize.INTEGER,
